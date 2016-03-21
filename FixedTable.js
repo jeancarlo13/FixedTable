e22c10c1f4f876bd85446ebca325bf5894ad96d7
jeancarlo13 <jeancarlo13@users.noreply.github.com>
2016-03-21 15:14:50 -0700

    Internet Explorer fixed
    Add support for Internet Explorer  o superior

--- FixedTable.js
+++ FixedTable.js
@@ -5,8 +5,8 @@
  * @Author:         jeancarlo13
  * @Lisense:        MIT Lisence (https://opensource.org/licenses/MIT)
  */
-var fixedTable = fixedTable || (function(){
-    'use strict';    
+var fixedTable = fixedTable || (function() {
+    'use strict';
     /*
      * Represent the sides of an element    
      */
@@ -25,18 +25,18 @@ var fixedTable = fixedTable || (function(){
      *      {Element}   parent: The parent element with scroll
      *      {bool}      oldState:   The last visibility state registred    
      */
-    var _tables = [];    
+    var _tables = [];
     /*
      * Check if the client rectangle is visible on screen
      * @param {DOM element} element:    The DOM element you want to know the visibility
      * @param {int|_sides}  side:       The site you want desired know the visibility.
      * @param {DOM element} parent:   The element that contains to the table and have scroll in y    
      */
-    function isElementInViewport (element, side, parent) {
+    function isElementInViewport(element, side, parent) {
         var rect = element.getBoundingClientRect();
         var parentRect = parent.getBoundingClientRect();
         var parentTop = 0 + parentRect.top;
-        
+
         if (side === _sides.Top) {
             return rect.top >= parentTop;
         } else if (side === _sides.Right) {
@@ -47,22 +47,22 @@ var fixedTable = fixedTable || (function(){
             return rect.left >= 0;
         } else if (side === _sides.Any) {
             return rect.top >= parentTop
-                || rect.left >= 0 
+                || rect.left >= 0
                 || rect.bottom <= (parent.innerHeight || parent.clientHeight) /*or $(window).height() */
                 || rect.right <= (parent.innerWidth || parent.clientWidth); /*or $(window).width() */
         } else if (side === _sides.AnyVerticalPart) {
-            return rect.bottom >= 0 
+            return rect.bottom >= 0
                 || (
-                    rect.top >= parentTop 
+                    rect.top >= parentTop
                     && rect.top <= (parent.innerHeight || parent.clientHeight) //or $(window).height()
                 );
         } else {
             // console.log('all', { top: rect.top, left: rect.left, bottom: rect.bottom, right: rect.right});
             return rect.top >= parentTop
-                && rect.left >= 0 
+                && rect.left >= 0
                 && rect.bottom <= (parent.innerHeight || parent.clientHeight) /*or $(window).height() */
                 && rect.right <= (parent.innerWidth || parent.clientWidth); /*or $(window).width() */
-        }                
+        }
     }
     /*
      * Review the visibility of the table into the parent with scroll
@@ -70,69 +70,84 @@ var fixedTable = fixedTable || (function(){
      *                          {Table}     table:  The table that you want fix the THead
      *                          {Element}   parent: The parent element with scroll
      *                          {bool}      oldState:   The last visibility state registred          
-     */        
-    function reviewScroll(obj){
+     */
+    function reviewScroll(obj) {
         var thead = obj.table.querySelector('thead');
-     
-        var isVisible = 
-            ( 
+        var tbodys = Array.prototype.slice.call(obj.table.querySelectorAll('tbody'));
+
+        var isVisible =
+            (
                 (
-                    isElementInViewport(obj.table, _sides.Top, obj.parent) 
+                    isElementInViewport(obj.table, _sides.Top, obj.parent)
                     || !isElementInViewport(obj.table, _sides.AnyVerticalPart, obj.parent)
-                ) 
+                )
                 && isElementInViewport(thead, _sides.All, obj.parent)
-            ) 
-            || obj.parent.scrollTop === 0
+            )
+            || obj.parent.scrollTop <= 16
             ;
         var parentTop = obj.parent.getBoundingClientRect().top + 'px';
-                        
         if (obj.oldState !== isVisible) {
-            thead.style.width = isVisible === true ? null : thead.offsetWidth + 'px';
+            thead.style.position = isVisible === true ? '' : 'fixed';
             
             var ths = Array.prototype.slice.call(thead.querySelector('tr').querySelectorAll('th, td'));
-            ths.forEach(function(th, index){
-                th.style.width = isVisible === true ? null : th.offsetWidth + 'px';
+
+            var widths = [];
+            if (isVisible !== true) {
+                var tds = Array.prototype.slice.call(tbodys[0].querySelector('tr').querySelectorAll('th, td'));
+                tds.forEach(function(td, index) {
+                     widths.push(td.offsetWidth);
+                });
+            }
+            
+            ths.forEach(function(th, index) {
+                th.style.width = isVisible === true ? '' : widths[index] + 'px';
             })
-            
-            thead.style.top = isVisible === true ? null : parentTop;                    
-            thead.style.position = isVisible === true ? null : 'fixed';
-            
+
+            tbodys.forEach(function(body) {
+                var tds = Array.prototype.slice.call(body.querySelector('tr').querySelectorAll('th, td'));
+                tds.forEach(function(td, index) {
+                    td.style.width = isVisible === true ? '' : widths[index] + 'px';
+                });
+            });
+
+            thead.style.width = isVisible === true ? '' : obj.table.offsetWidth + 'px';
+            thead.style.top = isVisible === true ? '' : parentTop;
         } else if (!isVisible && thead.style.top !== parentTop) {
             thead.style.top = parentTop
         }
-        
+
         obj.oldState = isVisible;
     }
     /*
     * Fix the thead of the desired table if this is not visible
     * @param {DOM element} table:    The table of the you want fix the thead
     * @param {DOM element} parent:   The element that contains to the table and have scroll in y
-    */   
-    function fixedTable(table, parent){
+    */
+    function fixedTable(table, parent) {
         var tableObj = {
             table: table,
             parent: parent,
             oldState: true
         };
         _tables.push(tableObj);
-        
+
         tableObj.parent.addEventListener(
-            'scroll', 
-            function(){ 
-                reviewScroll(tableObj); 
-            }, 
+            'scroll',
+            function() {
+                reviewScroll(tableObj);
+            },
             true);
-            
+
         tableObj.parent.scrollTop = 0;
     }
-    
+
     /*
     * Fix the thead of the desired table if this is not visible
     * @param {DOM element}  table:      The table of the you want fix the thead
     * @param {string}         maxWidth:   The max width of the container element, sample: 100px, 100%
     * @param {string}         maxHeight:  The max height of the container element, sample: 100px, 100%
-    */   
-    function fixedTableWithoutContainer(table, maxWidth, maxHeight){
+    */
+    function fixedTableWithoutContainer(table, maxWidth, maxHeight) {
         var parent = document.createElement('div');
         parent.style.width = maxWidth;
         parent.style.height = maxHeight;
@@ -141,17 +156,51 @@ var fixedTable = fixedTable || (function(){
         parent.appendChild(table);
         fixedTable(table, parent);
     }
-    
+    /**
+     * Overwrites obj1's values with obj2's and adds obj2's if non existent in obj1
+     * @param   {object}    obj1
+     * @param   {object}    obj2
+     * @returns {object}    A new object based on obj1 and obj2
+     */
+    function merge(obj1, obj2) {
+        var obj3 = {};
+        for (var attrname in obj1) {
+            obj3[attrname] = obj1[attrname];
+        }
+        for (var attrname in obj2) {
+            obj3[attrname] = obj2[attrname];
+        }
+        return obj3;
+    }
+
     // add an event handler to the document scroll for adjust the position of the fixed THead
     document.addEventListener(
-        'scroll', 
-        function(){ 
-            _tables.forEach(reviewScroll); 
-        }, 
+        'scroll',
+        function() {
+            _tables.forEach(reviewScroll);
+        },
         true);
-    
-    return {
-        simple: fixedTable,
-        WithoutContainer: fixedTableWithoutContainer
+
+    // return {
+    //     simple: fixedTable,
+    //     WithoutContainer: fixedTableWithoutContainer
+    // };
+    return function(options) {
+        var config = {
+            table: undefined,
+            container: undefined,
+            maxWidth: '100%',
+            maxHeight: '300px'
+        };
+
+        config = merge(config, options);
+
+        if (config.table === undefined) {
+            throw "Table element not provided";
+        } else if (config.container) {
+            fixedTable(config.table, config.container);
+        } else {
+            fixedTableWithoutContainer(config.table, config.maxWidth, config.maxHeight);
+        }
     };
 })();
